ac0b6f5e79c53bb27468a5d897fd84ce0aeb9e36
Jeff Lin <fishtailwrinkle@gmail.com>
2017-12-26 16:14:32 -0800

    Added .travis.yml

--- test/test-server.js
+++ test/test-server.js
@@ -5,3 +5,15 @@ const {app, runServer, closeServer} = require('../server');
 
 const should = chai.should();
 
+chai.use(chaiHttp);
+
+describe('Blog Posts', function() {
+	before(function() {
+		return runServer();
+	});
+
+	after(function() {
+		return closeServer();
+	});
+});
+
